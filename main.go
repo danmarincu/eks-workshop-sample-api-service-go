4a3f9db42e862a12ea036a8d72863f5c3d15f019
danmarincu <dan.marincu@gmail.com>
2022-11-20 19:08:02 +0100

    Update main.go

--- main.go
+++ main.go
@@ -15,7 +15,7 @@ func main() {
 
 		f := fib()
 
-		res := &response{Message: "Hello World"}
+		res := &response{Message: "Hello Crazy Changed World"}
 
 		for _, e := range os.Environ() {
 			pair := strings.Split(e, "=")
